e29e8f5120558881147716834c7b9e1cab10db05
Luiz Pilan <pilanluiz@gmail.com>
2022-10-13 22:48:30 +0100

    aligne items

--- js/flex.js
+++ js/flex.js
@@ -1,3 +1,13 @@
 function fd(valor){
     document.getElementById('container').style.flexDirection=valor;
 }
+
+function fw(valor){
+    document.getElementById('container').style.flexWrap=valor;
+}
+function fj(valor){
+    document.getElementById('container').style.justifyContent=valor;
+}
+function align(valor){
+    document.getElementById('container').style.alignItems=valor;
+}
